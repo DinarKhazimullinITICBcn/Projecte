5c652248cfceed56fbe9914e540c518fd1d44f9f
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-06 16:02:29 +0100

    Afegit totes les necessitats del sprint2

--- Sprint 2/projecte-sprint/src/main/java/com/example/demo/controllers/Controlador.java
+++ Sprint 2/projecte-sprint/src/main/java/com/example/demo/controllers/Controlador.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.bean.Empresa;
 import com.example.demo.repository.EmpresaRepository;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.List;
 
 @RestController
@@ -19,10 +20,22 @@ public class Controlador {
         return empresaRepository.save(newEmpresa);
     }
 
-    // Retorna empresas
-    @GetMapping("/empresa")
+    // Retorna totes les empresas
+    @GetMapping("/empreses")
     public List<Empresa> getAllEmpresas() {
         return empresaRepository.findAll();
+    }
+    // Retorna una empresa
+    @GetMapping("/empresa/{id}")
+    public Empresa getEmpresa(@PathVariable Long id) {
+        return empresaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Empresa no trobada amb la seguent id " + id));
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
     }
 
     // Modificar empresar
